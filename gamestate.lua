d995a0dc85c7b5981bf63e2d757988e31a0ba5f2
Gabriel Fondato <gabrielfondaato485@gmail.com>
2024-01-11 00:10:30 -0300

    colision added

--- gamestate.lua
+++ gamestate.lua
@@ -3,6 +3,16 @@ pipespawner = require("pipespawner")
 
 gamestate = {}
 
+function gamestate:checkColision()
+    if ((bird.x>Pipe1.getX()) and (bird.y>Pipe1.pipeBottom.topGapHeight)) then
+        print("bottom pipe collision")
+    end
+    if ((bird.x>Pipe1.getX()) and (bird.y<Pipe1.pipeTop.bottomGapHeight)) then
+        print("top pipe collision")
+    end
+end
+
+
 function gamestate:load()
     bird:load()
     pipespawner:load()
@@ -12,7 +22,8 @@ end
 function gamestate:update(dt)
     bird:update(dt)
     pipespawner:update(dt)
-
+    --print(Pipe1.x)
+    gamestate:checkColision()
 end
 
 function gamestate:draw()
